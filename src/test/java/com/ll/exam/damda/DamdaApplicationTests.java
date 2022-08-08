c8da55cd3bae3567d5af5b9f050f3cb74cfb7ed9
gimminjae <min356812@naver.com>
2022-08-08 23:42:11 +0900

    setting

---
+++ src/test/java/com/ll/exam/damda/DamdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.damda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

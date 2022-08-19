3ed3ecc1c91bf66f95b83f86ec6f7a8cc62630f0
jdi022222 <97517890+jdi022222@users.noreply.github.com>
2022-08-20 04:25:47 +0900

    스프링 시큐리티 설정

---
+++ src/main/java/com/ll/exam/damda/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.ll.exam.damda;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll()
+                .and()
+                .csrf().ignoringAntMatchers("/h2-console/**")
+                .and()
+                .headers()
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+        ;
+        return http.build();
+    }
+}

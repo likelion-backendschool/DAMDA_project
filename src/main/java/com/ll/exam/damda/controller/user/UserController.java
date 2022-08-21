d1e16f2a97537d259094565517299c0915f8e15f
jdi022222 <97517890+jdi022222@users.noreply.github.com>
2022-08-21 20:57:20 +0900

    navbar, form_errors 생성, 패스워드 일치 확인 controller 로직 추가, 회원가입 기능 구현

--- src/main/java/com/ll/exam/damda/controller/user/UserController.java
+++ src/main/java/com/ll/exam/damda/controller/user/UserController.java
@@ -1,23 +1,43 @@
 package com.ll.exam.damda.controller.user;
 
+import com.ll.exam.damda.form.user.UserCreateForm;
+import com.ll.exam.damda.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    private final UserService userService;
+
     @GetMapping("/signup")
-    public String signup() {
+    public String signup(UserCreateForm userCreateForm) {
         return "signup_form";
     }
 
     @PostMapping("/signup")
-    public String signup(String Form) {
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+
+        if (!userCreateForm.getPassword().equals(userCreateForm.getPassword_check())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect",
+                    "2개의 패스워드가 일치하지 않습니다.");
+            return "signup_form";
+        }
+
+        userService.create(userCreateForm.getUsername(),userCreateForm.getNickname(),
+                userCreateForm.getEmail(), userCreateForm.getPassword());
+
         return "redirect:/";
     }
 

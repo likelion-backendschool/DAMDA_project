e90a37b8a6dcb023526c394531e430cf6fba881b
NeoGuRi95 <jea5158@gmail.com>
2022-08-31 17:41:30 +0900

    SpotServiceTest 코드 추가

---
+++ src/test/java/com/ll/exam/damda/SpotServiceTest.java
@@ -0,0 +1,33 @@
+package com.ll.exam.damda;
+
+import com.ll.exam.damda.dto.search.spot.SpotDto;
+import com.ll.exam.damda.service.search.spot.SpotService;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpotServiceTest {
+
+    @Autowired
+    SpotService spotService;
+
+    @Test
+    @DisplayName("Spot여러개 조회시 Review가 N1 쿼리가_발생한다")
+    public void findAllTest() throws Exception {
+        //given
+        Page<SpotDto> spotList = spotService.getSpotListBy("", new ArrayList<>(), 0);
+
+        //then
+        assertThat(spotList.getNumberOfElements()).isEqualTo(8);
+    }
+}

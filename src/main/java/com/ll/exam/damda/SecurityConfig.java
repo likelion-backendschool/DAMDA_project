bffd3d75b79c32f31b8308bec6bdea109023ccd7
jdi022222 <97517890+jdi022222@users.noreply.github.com>
2022-09-06 01:54:46 +0900

    자동 로그인을 위한 remember-me 토큰 생성 구현

--- src/main/java/com/ll/exam/damda/SecurityConfig.java
+++ src/main/java/com/ll/exam/damda/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.ll.exam.damda;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,13 +11,27 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+        repo.setDataSource(dataSource);
+        return repo;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -34,12 +50,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
                 .logoutSuccessUrl("/")
                 .invalidateHttpSession(true)
+                .deleteCookies("remember-me", "JSESSIONID")
                 .and()
                 .rememberMe()
                 .key("rem-me-key")
-                .rememberMeParameter("remember") //Name of checkbox at login page
-                .rememberMeCookieName("rememberlogin") //Cookie name
-                .tokenValiditySeconds(7 * 24 * 60 * 60); // expiration time: 7 days // Remember login credentials for number of seconds
+                .rememberMeParameter("remember-me") //Name of checkbox at login page
+                .rememberMeCookieName("remember-login") //Cookie name
+                .tokenRepository(persistentTokenRepository()) //DataSource 추가
+                .tokenValiditySeconds(7 * 24 * 60 * 60);// expiration time: 7 days // Remember login credentials for number of seconds
     }
 
     @Bean
@@ -51,5 +69,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
+
 
 }

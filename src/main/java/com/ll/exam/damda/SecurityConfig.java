20b6946abd072b192a13427e822c7811fe2f32ba
jdi022222 <97517890+jdi022222@users.noreply.github.com>
2022-09-07 17:30:19 +0900

    SecurityConfig 주석 추가

--- src/main/java/com/ll/exam/damda/SecurityConfig.java
+++ src/main/java/com/ll/exam/damda/SecurityConfig.java
@@ -23,12 +23,15 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    // db 정보
     @Autowired
     private DataSource dataSource;
 
+    //userDetailsService를 상속받은 service
     @Autowired
     private UserSecurityService userSecurityService;
 
+    // JDBC 기반의 tokenRepository 구현체
     @Bean
     public PersistentTokenRepository persistentTokenRepository() {
         JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
@@ -36,33 +39,44 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return repo;
     }
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/**").permitAll()
-                .and() // 문맥의 끝
-                .csrf().ignoringAntMatchers("/h2-console/**")
-                .and() // 문맥의 끝
+
+                .and()
                 .headers().addHeaderWriter(new XFrameOptionsHeaderWriter(
                         XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+
                 .and()
                 .formLogin()
                 .loginPage("/user/login")
                 .defaultSuccessUrl("/")
+
                 .and()
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
                 .logoutSuccessUrl("/")
                 .invalidateHttpSession(true)
+                // 로그아웃시 자동로그인 쿠키와 세션 제거
                 .deleteCookies("remember-me", "JSESSIONID")
+
+                // 자동로그인 구현을 위한 rememberMe 설정
                 .and()
                 .rememberMe()
                 .key("rem-me-key")
+                // userDetailsService를 구현한 service를 가져와야 함
                 .userDetailsService(userSecurityService)
-                .rememberMeParameter("remember-me") //Name of checkbox at login page
-                .rememberMeCookieName("remember-login") //Cookie name
-                .tokenRepository(persistentTokenRepository()) //DataSource 추가
-                .tokenValiditySeconds(7 * 24 * 60 * 60);// expiration time: 7 days // Remember login credentials for number of seconds
+                // 로그인 html 페이지에서 이용할 id값
+                // 이 값은 default이며 변경할 시 로그인 페이지의 id 값도 수정
+                .rememberMeParameter("remember-me")
+                // 쿠키의 이름, f12 > application에서 확인 가능
+                .rememberMeCookieName("remember-login")
+                // 쿠키의 값을 저장할 DataSource
+                .tokenRepository(persistentTokenRepository())
+                // 쿠키의 유효기간, 7일을 계산함
+                .tokenValiditySeconds(7 * 24 * 60 * 60);
     }
 
     @Bean

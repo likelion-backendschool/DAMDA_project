5b019a94e559e53c9f3f014ec6770eba02d36cde
NeoGuRi95 <jea5158@gmail.com>
2022-09-15 21:38:55 +0900

    로그인한 유저가 포함된 계획만 조회될 수 있도록 장바구니 담기 로직 수정

--- src/main/java/com/ll/exam/damda/controller/search/spot/SpotController.java
+++ src/main/java/com/ll/exam/damda/controller/search/spot/SpotController.java
@@ -1,10 +1,13 @@
 package com.ll.exam.damda.controller.search.spot;
 
 import com.ll.exam.damda.dto.search.spot.SpotDto;
+import com.ll.exam.damda.entity.UserPlan;
 import com.ll.exam.damda.entity.design.map.Busket;
+import com.ll.exam.damda.entity.user.SiteUser;
 import com.ll.exam.damda.repository.design.map.BusketRepository;
 import com.ll.exam.damda.service.design.map.PlanService;
 import com.ll.exam.damda.service.search.spot.SpotService;
+import com.ll.exam.damda.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -15,7 +18,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @PreAuthorize("isAuthenticated()")
@@ -25,6 +30,7 @@ public class SpotController {
 
     private final SpotService spotService;
     private final PlanService planService;
+    private final UserService userService;
 
     @GetMapping("/detail")
     public String showSpotDetail(@RequestParam("spot") Long spotId, Model model) {
@@ -44,9 +50,11 @@ public class SpotController {
     }
 
     @GetMapping("/addBusket")
-    public String addSpotAtBusket(@RequestParam(value = "spotId") Long spotId, Model model) {
+    public String addSpotAtBusket(@RequestParam(value = "spotId") Long spotId, Model model, Principal principal) {
+        SiteUser siteUser = userService.getUser(principal.getName());
+        Set<UserPlan> userPlans = siteUser.getUserPlanSet();
         model.addAttribute("spotDto", spotService.findById(spotId));
-        model.addAttribute("plans", planService.getAllPlan());
+        model.addAttribute("userPlans", userPlans);
         return "spot/addSpotAtBusket.html";
     }
 }

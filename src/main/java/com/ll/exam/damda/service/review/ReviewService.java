0dbf5152cfa27b748f4b0020a8492a994d382756
JeongHyeSoo <nailonly@naver.com>
2022-09-22 04:07:40 +0900

    기존 리스트 역순 정렬 적용

--- src/main/java/com/ll/exam/damda/service/review/ReviewService.java
+++ src/main/java/com/ll/exam/damda/service/review/ReviewService.java
@@ -6,9 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -19,7 +21,10 @@ public class ReviewService {
 
 
     public Page<Review> getList(int page) {
-        Pageable pageable = PageRequest.of(page, 10);
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("firstCreatedDate"));
+
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts)); // 한 페이지에 10까지 가능
 
         return this.reviewRepository.findAll(pageable);
 

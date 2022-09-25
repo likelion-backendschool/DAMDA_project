2b38481285b12f42477f0d2a47edf1b92bc4bc67
gimminjae <min356812@naver.com>
2022-09-26 01:42:28 +0900

    플래너 수정 페이지의 지도에 컨트롤러 추가 구현

--- src/main/resources/static/scripts/map.js
+++ src/main/resources/static/scripts/map.js
@@ -1,3 +1,4 @@
+
 window.onload = function () {
     console.log('getAllBusket');
     // getAllBusket(plan_id);
@@ -17,6 +18,17 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 
 // 지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption);
+
+// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+var mapTypeControl = new kakao.maps.MapTypeControl();
+
+// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+var zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
 // 장소 검색 객체를 생성합니다
 var ps = new kakao.maps.services.Places();
